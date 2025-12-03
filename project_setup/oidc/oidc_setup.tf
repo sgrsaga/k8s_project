274f2c0b00e0e0a8b133ec0c8f6af4bad1d0e341
sgrsaga <sgrsagara@gmail.com>
2025-12-03 14:17:54 +0530

    multi envs

--- project_setup/oidc/oidc_setup.tf
+++ project_setup/oidc/oidc_setup.tf
@@ -37,12 +37,14 @@ resource "aws_iam_role" "github_oidc_role" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:sgrsaga/k8s_project:environment:dev",
+              "repo:sgrsaga/k8s_project:environment:ppe",
+              "repo:sgrsaga/k8s_project:environment:prod",
+            ]
           },
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:sgrsaga/k8s_project:ref:refs/heads/main"
-          },
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:sgrsaga/k8s_project:environment:dev"
+            "token.actions.githubusercontent.com:sub" = "repo:sgrsaga/k8s_project:ref:refs/heads/main",
           }
         }
       }
