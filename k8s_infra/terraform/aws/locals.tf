ae8dbf8792391c6bda8623013af6b09d1155b590
sgrsaga <sgrsagara@gmail.com>
2025-12-24 17:42:49 +0530

    test

--- k8s_infra/terraform/aws/locals.tf
+++ k8s_infra/terraform/aws/locals.tf
@@ -7,7 +7,7 @@ locals {
   vpc_cidr   = var.vpc_cidr
 
   # VPC specific values
-  vpc_name             = "${var.env}-vpc"
+  vpc_name             = "${var.env}-vpc-demo"
   private_subnets_cidr = var.private_subnets_cidr
   public_subnets_cidr  = var.public_subnets_cidr
 
